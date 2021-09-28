dec5a3dde7162b75c012a837230c17ee428cc63f
gamefive <43110921+5game@users.noreply.github.com>
2021-09-28 12:41:17 +0900

    Add files via upload

---
+++ C#scripts/Wall_move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall_move : MonoBehaviour
+{
+    public float Wall_speed = 0;
+    void Start()
+    {
+        Destroy(gameObject, 30f);
+    }
+
+  
+    void Update()
+    {
+        transform.Translate(Wall_speed * Time.deltaTime, 0, 0);
+    }
+}
