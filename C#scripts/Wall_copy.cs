c823f5fcc99cf3277531fdb10190920e76086c2f
gamefive <43110921+5game@users.noreply.github.com>
2021-09-28 12:40:09 +0900

    Add files via upload

---
+++ C#scripts/Wall_copy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall_copy : MonoBehaviour    
+{
+    public GameObject wallPrefab;
+    public float span = 0;
+    public float range = 0;
+    float delta = 0;
+   
+    void Start()
+    {
+        
+    }  
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject go = Instantiate(wallPrefab) as GameObject;
+            //go.transform.position = new Vector3(0, Random.Range(-range, range), 0);
+            
+        }
+    }
+}
