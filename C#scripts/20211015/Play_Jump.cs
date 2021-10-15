9ad194bb9b4f5e83dfd4a9d4fb3baa1dbf75e6fc
gamefive <43110921+5game@users.noreply.github.com>
2021-10-15 18:08:12 +0900

    Add files via upload

---
+++ C#scripts/20211015/Play_Jump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Play_Jump : MonoBehaviour
+{
+    GameObject jumpNum; //
+    int score = 0;
+
+    public void JumpAddition()
+    {
+        this.score += 1;
+    }
+    void Start()
+    {
+        this.jumpNum = GameObject.Find("JumpNum_UiText");
+    }
+
+   
+    void Update()
+    {
+        this.jumpNum.GetComponent<Text>().text = this.score.ToString(); //text Ui 에 점수 출력
+    }
+}
